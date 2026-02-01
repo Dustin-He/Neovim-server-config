6051c6a0631d3d6345156ab273ff81b790c6e2ab
Dustin-He <16hjt@pku.edu.cn>
2026-02-01 15:12:15 +0800

    diasble open url for server lazy.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,33 +1,38 @@
 -- lua/config/lazy.lua
+local env = require('utils.env')
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  -- UI 设置
-  ui = { border = "rounded" },
-  -- 自动检查更新 (建议远程关闭，减少网络请求)
-  checker = { enabled = false },
-  change_detection = { notify = false },
-  -- 性能优化
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin", "netrwPlugin", -- 禁用 netrw (用 oil 代替)
-      },
+    -- UI 设置
+    ui = {
+        border = "rounded",
+        browser = env.is_ssh and "true" or "open"
     },
-  },
+    -- 自动检查更新 (建议远程关闭，减少网络请求)
+    checker = { enabled = false },
+    change_detection = { notify = false },
+    -- 性能优化
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin", "netrwPlugin", -- 禁用 netrw (用 oil 代替)
+            },
+        },
+    },
 })
